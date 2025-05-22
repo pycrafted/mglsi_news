181049f0a862625a1f249bb0f779ddfd4bb4130e
pycrafted <abdoulayelah@esp.sn>
2025-05-22 00:21:02 +0000

    fix the app

---
+++ config/db_connect.php
@@ -0,0 +1,15 @@
+<?php
+function getDatabaseConnection() {
+    $host = 'localhost';
+    $dbname = 'mglsi_news';
+    $username = 'root';
+    $password = '';
+
+    try {
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch(PDOException $e) {
+        die("Erreur de connexion : " . $e->getMessage());
+    }
+} 
